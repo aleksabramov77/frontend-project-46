a58a7ae2a2e66cf3c78081028fcb775d5111ecd9
Aleksandr Abramov <aleksandr.a@findbride.com>
2024-10-07 02:59:37 +0600

    003 fixtures arguments actions

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,23 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+import path from 'path';
+import fs from 'fs';
+
 const program = new Command();
+
+const action = (pathToFile1, pathToFile2) => {
+    const file1 = JSON.parse(fs.readFileSync(path.resolve(pathToFile1)))
+    const file2 = JSON.parse(fs.readFileSync(path.resolve(pathToFile2)))
+
+    console.log(file1, file2)
+
+}
 
 program
     .version('1.0.0')
     .description('Compares two configuration files and shows a difference.')
     .helpOption('-h, --help', 'output usage information')
     .option('-f, --format [type]',  'output format')
+    .arguments('<pathToFile1>, <pathToFile2>')
+    .action(action)
     .parse(process.argv);
